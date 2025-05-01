68712eb6b40bb1e899f86651595968104ea68888
Chintha Hareesh Kumar <chintha.kumar@innovaccer.com>
2025-05-01 17:10:38 +0530

    created a sample table

--- postgres/merchant_template/2025_04_07_15_59_00_paycio_merchant_release.sql
+++ postgres/merchant_template/2025_04_07_15_59_00_paycio_merchant_release.sql
@@ -3,4 +3,9 @@
 --changeset chhareeshkumar:1602251613-01 splitStatements:false
 CREATE SCHEMA IF NOT EXISTS merchant_template;
 
+--changeset chhareeshkumar:1602251613-02 splitStatements:false
+CREATE TABLE merchant_template.table_template (
+  ID INT PRIMARY KEY,
+  NAME VARCHAR(100)
+);
 
