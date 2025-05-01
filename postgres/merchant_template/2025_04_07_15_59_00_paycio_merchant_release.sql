253db993e67b44e2282693017dd225a66afeee29
Chintha Hareesh Kumar <chintha.kumar@innovaccer.com>
2025-05-01 17:11:31 +0530

    created a sample table minor

--- postgres/merchant_template/2025_04_07_15_59_00_paycio_merchant_release.sql
+++ postgres/merchant_template/2025_04_07_15_59_00_paycio_merchant_release.sql
@@ -6,6 +6,7 @@ CREATE SCHEMA IF NOT EXISTS merchant_template;
 --changeset chhareeshkumar:1602251613-02 splitStatements:false
 CREATE TABLE merchant_template.table_template (
   ID INT PRIMARY KEY,
-  NAME VARCHAR(100)
+  NAME VARCHAR(100),
+  DOB DATE
 );
 
