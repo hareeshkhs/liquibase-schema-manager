df9144152baaba7f5c0f223449ab47153aa9697c
Chintha Hareesh Kumar <chintha.kumar@innovaccer.com>
2025-05-01 17:23:56 +0530

    created a sample table bump_minor

--- postgres/core/2025_04_07_15_59_00_paycio_mvp_release.sql
+++ postgres/core/2025_04_07_15_59_00_paycio_mvp_release.sql
@@ -3,4 +3,11 @@
 --changeset chhareeshkumar:1602251613-01 splitStatements:false
 CREATE SCHEMA IF NOT EXISTS core;
 
+--changeset chhareeshkumar:1602251613-02 splitStatements:false
+CREATE TABLE core.table_core (
+  ID INT PRIMARY KEY,
+  NAME VARCHAR(100),
+  DOB DATE
+);
+
 
